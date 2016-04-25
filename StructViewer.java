789712c8da4a030daa9e84728967845ffc7537a9
underwanderer <makethedog@gmail.com>
2016-04-25 20:29:36 +0300

    Update StructViewer.java

--- StructViewer.java
+++ StructViewer.java
@@ -23,9 +23,38 @@ implements Runnable
   
   private static void createUI ( JFrame f ) {
     JPanel treeView = new JPanel (  );
-    TreeComponent treeComponent = new TreeComponent (  );
+    TreeComponentModel treeModel = new TreeComponentModel (  );
+    TreeComponent treeComponent = new TreeComponent ( treeModel );
     treeView .add ( treeComponent );
+    
     JPanel controls = new JPanel (  );
+    JButton createTree = new JButton (  );
+    JButton clear = new JButton (  );
+    JComboBox treeType = new JComboBox (  ); //random, progression, from file, empty
+    JCheckBox isSorted = new JCheckBox (  );
+    //Add this controls on the controls panel immidiately or woth subpanels
+    
+    //How to add this action to the bottons through constructor? 
+    Action createTreeAction = new Action (  ) {
+      public void actionPerformed ( ActionEvent e ) {
+        if ( e .getSource () == createTree )
+          treeModel .createNewTree (  );
+        else if ( e .getSource () == clear )
+          treeModel .clear (  );
+      }
+    }
+    
+    treeType .addItemChangedListener (
+      new StateChangedListerner (  ) {
+        public void stateChanged (ItemEvent e) {
+          treeModel .setTreeType ( e .getSelectedItem (  ) );
+        }
+      }
+    );
+    treeType .addActionListener ( createA);
+    
+    isSorted .add
+    
     f .setLayout ( LayoutManager .BORDER_LAYOUT );
     f .add ( treeView, BorderLayout .CENTER );
     f .add ( controls, BorderLayout .EAST );
